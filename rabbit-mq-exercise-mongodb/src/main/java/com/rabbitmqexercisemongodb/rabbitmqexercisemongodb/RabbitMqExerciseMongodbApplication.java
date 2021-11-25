f41d04bdf2366e8332c2e8c9968408f8ff0d014c
Mohammed <maxammed_1989@hotmail.com>
2021-11-25 20:20:44 +0100

    added db
    added mongodb

---
+++ rabbit-mq-exercise-mongodb/src/main/java/com/rabbitmqexercisemongodb/rabbitmqexercisemongodb/RabbitMqExerciseMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmqexercisemongodb.rabbitmqexercisemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqExerciseMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqExerciseMongodbApplication.class, args);
+	}
+
+}
---
+++ rabbit-mq-exercise-mongodb/src/test/java/com/rabbitmqexercisemongodb/rabbitmqexercisemongodb/RabbitMqExerciseMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmqexercisemongodb.rabbitmqexercisemongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqExerciseMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
