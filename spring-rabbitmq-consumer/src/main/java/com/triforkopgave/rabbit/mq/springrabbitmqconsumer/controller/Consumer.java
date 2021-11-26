7556d9fbe188532d647afe78929b50fe62ce07d8
Mohammed <maxammed_1989@hotmail.com>
2021-11-26 21:37:59 +0100

    using channels now
    using channels to read from the queue.

--- spring-rabbitmq-consumer/src/main/java/com/triforkopgave/rabbit/mq/springrabbitmqconsumer/controller/Consumer.java
+++ spring-rabbitmq-consumer/src/main/java/com/triforkopgave/rabbit/mq/springrabbitmqconsumer/controller/Consumer.java
@@ -1,36 +1,49 @@
 package com.triforkopgave.rabbit.mq.springrabbitmqconsumer.controller;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.GetResponse;
 import com.triforkopgave.rabbit.mq.springrabbitmqconsumer.model.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 @Slf4j
 public class Consumer {
     @RabbitListener(queues = "queue.A")
-    private void receive(Message message) throws Exception {
+    private void receive(Message message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag)  throws IOException { {
         try {
             long timeStamp = System.currentTimeMillis() - message.getTime_ms();
             log.info("Message received from Queue queue.A->{}", timeStamp);
             // if (timeStamp > 1000)   then we do nothing
 
            if(timeStamp <=1000 & timeStamp % 2==0 ){} // gem i mongodb
-            else{
+            else {
 
-                message.setTime_ms(System.currentTimeMillis());   // set new timeStamp
-                throw  new Exception(); // throw error to requeue the msg.
+               message.setTime_ms(System.currentTimeMillis());   // set new timeStamp
+
+               GetResponse gr = channel.basicGet("some.queue", false);
+
+               channel.basicNack(gr.getEnvelope().getDeliveryTag(), false, true);
+
            }
+            
 
 
                 }catch(Exception e){
+
+        }
 
             log.info("timestamp of msg was an less than 1 minute and uneven  so msg needs to be requeue");
 
                 }
 
 
-    /* opgaven: 
+    /* opgaven:
     Producer app: Producere en besked med en timestamp i et gentage loop.
 Consumer app:
 Tag besked af køen. Hvis beskedens timestamp er over 1 minut gammel – så smid den væk. (ikke gem den i db)
--- spring-rabbitmq-producer/src/main/java/com/triforkopgave/rabbit/mq/springrabbitmqproducer/configuaration/RabbitMQConfig.java
+++ spring-rabbitmq-producer/src/main/java/com/triforkopgave/rabbit/mq/springrabbitmqproducer/configuaration/RabbitMQConfig.java
@@ -1,15 +1,21 @@
 package com.triforkopgave.rabbit.mq.springrabbitmqproducer.configuaration;
 
+import com.triforkopgave.rabbit.mq.springrabbitmqproducer.model.Message;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.handler.annotation.Header;
+
+import java.nio.channels.Channel;
 
 
 @Configuration
@@ -50,7 +56,11 @@ public class RabbitMQConfig {
 
     }
 
+    @RabbitListener(queues = queueName)
+    public void receiveMessage(Message msg, Channel channel,
+                               @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws Exception {
 
+    }
 
 
 }
